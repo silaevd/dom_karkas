6d3dc2cd7ebd0f840874d5d0f3298b954174f3e8
SilaevD <silaevd@yandex.ru>
2021-04-04 20:31:01 +0300

    sketch

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,26 @@
-$(function () {
-  $('.images').fotorama();
+$(window).on("load", function() {
+
+  //-------------------
+  //* Fotorama
+  //-------------------
+  $(function () {
+    $('.images').fotorama();
+  });
+
+  //-------------------
+  //* ScrollToId
+  //-------------------
+
+  $(function() {
+    $('a[href^="#"]').click(function(e) {
+      e.preventDefault();
+      var target = $(this).attr('href');
+      var stop = $(target).offset().top;
+      var delay = 1000;
+      $('html, body').animate({scrollTop: stop + 'px'}, delay);
+    });
+  });
+
+  SmoothScroll({ stepSize: 40 });
+
 });
