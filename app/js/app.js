b59073dbc98f5d684c884f56730b222b74ddae9c
SilaevD <silaevd@yandex.ru>
2021-04-05 21:46:19 +0300

    nav width

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,34 @@
+//-------------------
+//* Nav
+//-------------------
+
+function navResize() {
+  const header = $('header');
+  const nav = $('nav');
+  let headerWidth = header.width();
+  nav.css('width', headerWidth);
+}
+
+navResize();
+
+$(window).resize(function(){
+  navResize();
+  console.log(headerWidth);
+});
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>25){
+    $('nav').addClass('nav_fixed');
+  }
+  else if ($(this).scrollTop()<25){
+    $('nav').removeClass('nav_fixed');
+  }
+});
+
+//-------------------
+//* OnLoad
+//-------------------
+
 $(window).on("load", function() {
 
   //-------------------
@@ -17,10 +48,19 @@ $(window).on("load", function() {
       var target = $(this).attr('href');
       var stop = $(target).offset().top;
       var delay = 1000;
-      $('html, body').animate({scrollTop: stop + 'px'}, delay);
+      $('html, body').animate({scrollTop: stop - 49 +'px'}, delay);
     });
   });
 
+  //-------------------
+  //* Phone Mask
+  //-------------------
+
+  // $('input[name="phone"]').inputmask({"mask": "+7(999) 999-9999"});
+
+  //-------------------
+  //* SmoothScroll
+  //-------------------
   SmoothScroll({ stepSize: 40 });
 
 });
