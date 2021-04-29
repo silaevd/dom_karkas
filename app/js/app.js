87d0ea2ba00798006ea7fc0f648d1acee4149769
SilaevD <silaevd@yandex.ru>
2021-04-29 22:33:58 +0300

    text

--- app/js/app.js
+++ app/js/app.js
@@ -92,10 +92,10 @@ $(window).on("load", function() {
   });
 
   /* ---------------------------------------------- /*
-         * CallMe PopUp
-    /* ---------------------------------------------- */
+  * CallMe PopUp
+  /* ---------------------------------------------- */
 
-  $('#callToMe').magnificPopup({
+  $('.callToMe').magnificPopup({
 
     items: {
       src: $('#callMePopUp'),
@@ -108,7 +108,7 @@ $(window).on("load", function() {
   });
 
   /* ---------------------------------------------- /*
-         * Goods PopUp
+  * Goods PopUp
   /* ---------------------------------------------- */
 
   $('.project').click(function() {
@@ -119,94 +119,127 @@ $(window).on("load", function() {
     const desc = this.querySelector('.project-info').innerHTML;
     const popUp = document.querySelector('.goodsPopUp');
     const imgList = [{
-        img: './images/goods/' + dataName + '/1.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/2.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/3.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/4.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/5.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/6.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/7.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/8.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/9.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/10.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/11.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/12.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/13.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/14.jpg'
-      },
-      {
-        img: './images/goods/' + dataName + '/15.jpg'
-      },
-    ];
-
-    popUp.querySelector('.goodsPopUp__title').textContent = title;
-    popUp.querySelector('input[name="project_name"]').setAttribute('value', title);
-    popUp.querySelector('.goodsPopUp__desc').innerHTML = desc;
-    popUp.querySelector('.goodsPopUp__price').querySelector('.highlight').textContent = price;
-    popUp.querySelector('.goodsPopUp__cover').innerHTML = '<div id="fotorama" class="fotorama" data-auto="false"></div>';
-
-    $('#fotorama').fotorama({
-      nav: 'thumbs',
-      allowfullscreen: 'true',
-      arrows: 'true',
-      // click: 'true',
-      loop: 'true',
-      thumbwidth: '110',
-      thumbheight: '60',
-      width: '100%',
-      height: 'auto',
-      maxheight: '377',
-      data: imgList
-    });
-
-  });
-
-  $('.project').magnificPopup({
-    items: {
-      src: $('#goodsPopUp'),
-      type: 'inline'
+      img: './images/goods/' + dataName + '/1.jpg'
     },
-    preloader: false,
-    showCloseBtn: false,
-    autoFocusLast: true,
-    fixedContentPos: false,
+    {
+      img: './images/goods/' + dataName + '/2.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/3.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/4.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/5.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/6.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/7.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/8.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/9.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/10.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/11.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/12.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/13.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/14.jpg'
+    },
+    {
+      img: './images/goods/' + dataName + '/15.jpg'
+    },
+  ];
+
+  popUp.querySelector('.goodsPopUp__title').textContent = title;
+  popUp.querySelector('input[name="project_name"]').setAttribute('value', title);
+  popUp.querySelector('.goodsPopUp__desc').innerHTML = desc;
+  popUp.querySelector('.goodsPopUp__price').querySelector('.highlight').textContent = price;
+  popUp.querySelector('.goodsPopUp__cover').innerHTML = '<div id="fotorama" class="fotorama" data-auto="false"></div>';
+
+  $('#fotorama').fotorama({
+    nav: 'thumbs',
+    allowfullscreen: 'true',
+    arrows: 'true',
+    // click: 'true',
+    loop: 'true',
+    thumbwidth: '110',
+    thumbheight: '60',
+    width: '100%',
+    height: 'auto',
+    maxheight: '377',
+    data: imgList
   });
 
+});
 
-  /* ---------------------------------------------- /*
-       * PopUp close
-  /* ---------------------------------------------- */
+$('.project').magnificPopup({
+  items: {
+    src: $('#goodsPopUp'),
+    type: 'inline'
+  },
+  preloader: false,
+  showCloseBtn: false,
+  autoFocusLast: true,
+  fixedContentPos: false,
+});
 
-  $('.popUp__close').on('click', function(e) {
-    e.preventDefault();
-    $.magnificPopup.close();
+
+/* ---------------------------------------------- /*
+* PopUp close
+/* ---------------------------------------------- */
+
+$('.popUp__close').on('click', function(e) {
+  e.preventDefault();
+  $.magnificPopup.close();
+});
+
+/* ---------------------------------------------- /*
+* Form submit
+/* ---------------------------------------------- */
+
+$('form').on('submit', function () {
+
+  const form = $(this);
+  const submitBtn = form.find('button[type="submit"]');
+
+  $.ajax({
+    type: "POST",
+    url: "mail.php",
+    data: form.serialize()
+  }).done(function() {
+    $.magnificPopup.open({
+      items: {
+        src: $('#thankyouPopUp'),
+        type: 'inline',
+      },
+      preloader: false,
+      showCloseBtn: false,
+
+    });
+    setTimeout(function() {
+      // Done Functions
+      form.trigger("reset");
+      $.magnificPopup.close();
+    }, 4000);
   });
+  return false;
+});
+
 
 
 });
